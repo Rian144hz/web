cd81955e03f7d370397dc6cf078f348d0f8a71ab
rian144hz <rianmatheus669@gmail.com>
2026-05-20 12:15:22 -0300

    Iniciando as aulas de javascript

---
+++ TecnologiasWeb/aula-15/first.js
@@ -0,0 +1,16 @@
+const pi = 3.14;
+var nome = "Matheus";
+var curso = "cc";
+var turno = "Noturno";
+
+console.log("Valor de pi: "+pi);
+console.log("Nome do aluno: "+nome);
+console.log("Curso: "+curso);
+console.log("Turno: "+turno);
+
+
+
+
+var frutas = ["Maçã","Banana","Manga","Maracuja"];
+frutas.filter(frutas => frutas.contains("M"))
+console.log(frutas)
